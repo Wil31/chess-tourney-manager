ac7d630f74eab585fe0ae466ac6baaaa63a42e1f
[William] <w.gavigniaux@gmail.com>
2022-01-17 18:44:06 +0100

    Tests class improvements

--- tests/tests.py
+++ tests/tests.py
@@ -22,55 +22,55 @@ def genere_resultats_alea(tour):
         match.ajouter_resultats_match(resultat_j1, resultat_j2)
 
 
+def cree_joueurs_alea(nombre):
+    """
+    Créer x joueurs aléatoires
+    """
+    liste_joueurs = []
+    for i in range(nombre):
+        liste_joueurs.append(Joueur(names.get_last_name(),
+                                    names.get_first_name(),
+                                    random.randint(1300, 2700)))
+    return liste_joueurs
+
+
 class Tests:
     def __init__(self, tournoi):
         self.tournoi = tournoi
 
-    def cree_joueurs_alea(self, nombre):
-        """
-        Créer x joueurs aléatoires
-        """
-        for i in range(nombre):
-            self.tournoi.joueurs.append(
-                Joueur(names.get_last_name(), names.get_first_name(),
-                       random.randint(1300, 2700)))
-
-    def run(self):
-        self.cree_joueurs_alea(8)
-
-        # Créer un tour
-        tour_1 = Tour("Tour n°1", "15 janvier", "17:11", self.tournoi)
-        # Générer les paires et les premiers matchs
-        tour_1.generer_paires_initial()
-
-        genere_resultats_alea(tour_1)
-
-        # Affiche les infos du tournoi
-        print(self.tournoi)
+    def affichage_tour(self, tour):
         print("==========================================================")
-        print(tour_1)
+        print(tour)
         print()
         # Affiche les infos des joueurs
         for joueur in self.tournoi.joueurs:
             print(joueur)
         print()
         # Affiche les infos des matchs du tour 1
-        for match in tour_1.liste_matchs:
+        for match in tour.liste_matchs:
             print(match)
+        print()
+
+    def run(self):
+        self.tournoi.joueurs = cree_joueurs_alea(8)
+        # Créer un tour
+        tour_1 = Tour("Tour n°1", "15 janvier", "17:11", self.tournoi)
+        # Générer les paires et les premiers matchs
+        tour_1.generer_paires_initial()
+        genere_resultats_alea(tour_1)
+
+        # Affiche les infos du tournoi
+        print(self.tournoi)
+        self.affichage_tour(tour_1)
 
         # Créer un tour 2
         tour_2 = Tour("Tour n°2", "15 janvier", "17:30", self.tournoi)
         tour_2.generer_paires()
-
         genere_resultats_alea(tour_2)
+        self.affichage_tour(tour_2)
 
-        print("==========================================================")
-        print(tour_2)
-        print()
-        # Affiche les infos des joueurs
-        for joueur in self.tournoi.joueurs:
-            print(joueur)
-        print()
-        # Affiche les infos des matchs du tour 2
-        for match in tour_2.liste_matchs:
-            print(match)
+        # Créer un tour 3
+        tour_3 = Tour("Tour n°3", "15 janvier", "17:40", self.tournoi)
+        tour_3.generer_paires()
+        genere_resultats_alea(tour_3)
+        self.affichage_tour(tour_3)
